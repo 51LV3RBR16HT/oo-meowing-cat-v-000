7d7251cb0866d7f9c535855b5ea60f4ef91940e1
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-14 18:45:44 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,10 +1,11 @@
 ## code your solution here.
 class Cat
-  attr_accessor :name :meow
+  attr_accessor :name
   cat_name = Cat.new
   cat_name = "Maru"
 
-  maru_meow = Cat.new
-  maru_meow = "Meow"
-  puts maru_meow
+  def meow
+    puts "meow"
+  end
+  
 end
